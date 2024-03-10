985c96d252bbaaeb5c4c8bca43f1a4a3b7ec9cee
Y-SOLAR <162905652+Y-SOLAR@users.noreply.github.com>
2024-03-10 18:25:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 10 16:45:33 2024
+import streamlit as st
 
-@author: Pasci
-"""
-
-
-# Create an empty container
 placeholder = st.empty()
 
-actual_email = "email"
-actual_password = "password"
+# Replace the placeholder with some text:
+placeholder.text("Hello")
 
-# Insert a form in the container
-with placeholder.form("login"):
-    st.markdown("#### Enter your credentials")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
 
-if submit:
-    if email == actual_email and password == actual_password:
-        placeholder.empty()
-        st.success("Login successful")
-        st.session_state.username = email
-        st.session_state.page = "home_page"
-    else:
-        st.error("Invalid credentials. Please try again.")
+# Replace the chart with several elements:
+with placeholder.container():
+    st.write("This is one element")
+    st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
